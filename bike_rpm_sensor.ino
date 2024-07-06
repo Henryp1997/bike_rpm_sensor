37c02b40e18b2707be105ad4480c599a4c44e65c
Henry Pickersgill <htap2@live.co.uk>
2024-07-06 22:05:51 +0100

    show calculated speed in km/h on display now. Seperate variables into header file

--- bike_rpm_sensor.ino
+++ bike_rpm_sensor.ino
@@ -1,20 +1,73 @@
-int zero[8] = {1, 1, 1, 1, 1, 1, 0};
-int one[8] = {0, 1, 1, 0, 0, 0, 0};
-int two[8] = {1, 1, 0, 1, 1, 0, 1};
-int three[8] = {1, 1, 1, 1, 0, 0, 1};
-int four[8] = {0, 1, 1, 0, 0, 1, 1};
-// int four[8] = {0, 0, 0, 0, 0, 0, 0};
-int five[8] = {1, 0, 1, 1, 0, 1, 1};
-int six[8] = {1, 0, 1, 1, 1, 1, 1};
-int seven[8] = {1, 1, 1, 0, 0, 0, 0};
-int eight[8] = {1, 1, 1, 1, 1, 1, 1};
-int nine[8] = {1, 1, 1, 1, 0, 1, 1};
+/*
+     6
+    ---
+11|     |7
+  |  12 |
+    ---
+10|     |8
+  |     |
+    ---
+     9
 
-int digit_pins[4] = {2, 3, 4, 5};
-int segment_pins[8] = {6, 7, 8, 9, 10, 11, 12, 13};
+  pin 13 is decimal point (probably don't need)
 
-int update_display = 0;
-int digit1 = 0; int digit2 = 0; int digit3 = 0;
+  D1 = 2
+  D2 = 3
+  D3 = 4
+  D4 = 5
+
+*/
+
+#include "variables.hpp"
+
+// setup function - called once at the start
+void setup() {
+  init_all_pins();
+  Serial.begin(9600);
+}
+
+// main loop
+void loop() {
+  set_display(speed);
+
+  if (init_loop) {
+    // read voltage across hall effect 
+    // sensor data pin to initialise variable
+    prev_voltage = dac_to_volts(analogRead(A6));
+    init_loop = 0;
+  }
+
+  float voltage = dac_to_volts(analogRead(A6));
+  if (magnet_detected(voltage)) {
+    // magnet detected, start measuring time
+    if (!measuring) {
+      // first detection of magnet
+      start_time = micros();
+    }
+    else {
+      // second detection of magnet
+      end_time = micros();
+      time_diff = end_time - start_time;
+
+      // calculated 50Hz as the maximum possible rotation speed
+      if ((time_diff / 1000000.0) > 0.1) {
+        measuring = 0;
+        speed = calculate_speed(time_diff);
+      }
+      
+    }
+  }
+  else {
+    // ensure that the analog pin went LOW at some point to count a valid revolution
+    if ((micros() - start_time) / 1000000.0 < 10) {
+      measuring = 1;
+    }
+    else {
+      measuring = 0;
+      // set_display(0.00);
+    }
+  }
+}
 
 int init_all_pins() {
   for (int i = 0; i < 9; i++) {
@@ -74,61 +127,70 @@ int set_number(int num, int decimal) {
   digitalWrite(segment_pins[7], decimal);
 }
 
+void set_display(float value) {
+  digit1 = floor(value);
+  if (value > 10) {
+    // for values greater than or equal to 10, the digit1 value
+    // is actually the first two digits
+    int digit1_new = floor(digit1 / 10);
+    int digit2_new = digit1 % 10;
+    int digit3_new = floor((value - digit1) * 10);
+    int digit4_new = floor((((value - digit1) * 10) - digit3_new) * 10);
+    digit1 = digit1_new; digit2 = digit2_new; digit3 = digit3_new; digit4 = digit4_new;
+  }
+  else {
+    digit2 = floor((value - digit1) * 10);
+    digit3 = floor((((value - digit1) * 10) - digit2) * 10);
+  }
+  
+  // set first digit
+  set_digit_pos(0, 1, 1, 1);
+  if (value > 10) {
+    set_number(digit1, false);
+  }
+  else {
+    set_number(digit1, true);
+  }
+  set_all_low();
+
+  // set second digit
+  set_digit_pos(1, 0, 1, 1);
+  if (value > 10) {
+    set_number(digit2, true);
+  }
+  else {
+    set_number(digit2, false);
+  }
+  set_all_low();
+  
+  // set third digit
+  set_digit_pos(1, 1, 0, 1);
+  set_number(digit3, false);
+  set_all_low();
+
+  // set fourth digit if needed
+  set_digit_pos(1, 1, 1, 0);
+  if (value > 10) {
+    set_number(digit4, false);
+    set_all_low();
+  }
+}
+
 void set_all_low() {
   for (int i = 0; i < 8; i++) {
     digitalWrite(segment_pins[i], 0);
   }
 }
 
-void setup() {
-  init_all_pins();
-  Serial.begin(9600);
+float dac_to_volts(float dac) {
+  return dac * 5.0 / 1024.0;
 }
 
-void loop () {
-  if (update_display == 200) {
-    // first read voltage from analog pin A6
-    float voltage = (analogRead(A6) * 5.0) / 1024.0;
-
-    digit1 = floor(voltage);
-    digit2 = floor((voltage - digit1) * 10);
-    digit3 = floor((((voltage - digit1) * 10) - digit2) * 10);
-    update_display = 0;
-  }
-  else {
-    update_display++;
-  }
-
-  set_digit_pos(0, 1, 1, 1);
-  set_number(digit1, true);
-  set_all_low();
-
-  set_digit_pos(1, 0, 1, 1);
-  set_number(digit2, false);
-  set_all_low();
-  
-  set_digit_pos(1, 1, 0, 1);
-  set_number(digit3, false);
-  set_all_low();
-
+bool magnet_detected(float voltage) {
+  return abs(voltage - prev_voltage) > noise_voltage;
 }
 
-/*
-     6
-    ---
-11|     |7
-  |  12 |
-    ---
-10|     |8
-  |     |
-    ---
-     9
-
-  pin 13 is decimal point (probably don't need)
-
-  D1 = 2
-  D2 = 3
-  D3 = 4
-  D4 = 5
-
-*/
+float calculate_speed(unsigned long time_diff) {
+  // see variables.h for derivation of speed_const
+  return speed_const / time_diff;
+}
