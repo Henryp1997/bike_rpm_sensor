1122cdaf74b77b123b563946638c5e9c9972a573
Henry Pickersgill <htap2@live.co.uk>
2025-05-17 17:40:38 +0100

    update main file, need to revisit logic

--- bike_rpm_sensor.ino
+++ bike_rpm_sensor.ino
@@ -1,29 +1,70 @@
-#include <LiquidCrystal.h>
+// #include <chrono>
+// using namespace std::chrono;
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
+int timer_lcd_voltage = 0;
+int measuring_rpm = 0;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+float prev_voltage = 0;
+const float noise_voltage = 0.05;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
-  lcd.print("Initialising...");
+  prev_voltage = get_sensor_voltage(analogRead(A0));
   delay(1000);
 }
 
-int timer_lcd_voltage = 0;
-void loop() {
-  float voltage = (analogRead(A0) * 5.0) / 1024.0;
-  // Serial.println(voltage);
-  int timer_check = timer_lcd_voltage % 50;
-  Serial.println(timer_check);
-  if (!timer_check) {
-    lcd.clear();
-    lcd.print(voltage);
-    lcd.print("V");
+int test() {
+  int rpms[100];
+  int i = 0;
+  while (i < 100) {
+    rpms[i] = i;
+    i ++;
   }
-  timer_lcd_voltage ++;
 }
+
+float get_sensor_voltage(float volt_bits) {
+  return (volt_bits * 5.0) / 1024.0;
+}
+
+// int calculate_rpm(int start_time, )
+
+void loop() {
+  int rpms = test();
+
+  // read HEF sensor voltage
+  float voltage = get_sensor_voltage(analogRead(A6));
+  Serial.println(voltage);
+
+  // 0.05V is about the maximum I could consider to be noise
+  Serial.println(noise_voltage);
+  if (abs(voltage - prev_voltage) > noise_voltage) {
+    // magnet detected, start measuring time from here
+    Serial.println("Magnet detected");
+    // int start_time = high_resolution_clock::now();
+    // if (measuring_rpm = 1) {
+      
+    // }
+    // measuring_rpm = 1;
+  }
+
+  // check if voltage has returned to the initial value
+  // i.e., magnet not close to sensor
+  if (abs(voltage - prev_voltage) < noise_voltage) {
+    // stop measuring time. Now calculate the difference in
+    // time from when the peak was detected to now, this is
+    // the time taken for one full revolution i.e. the RPM
+    Serial.println("Magnet no longer detected");
+  }
+
+  // only update LCD every 50th pass through this function
+  if (timer_lcd_voltage == 50) {
+    timer_lcd_voltage = -1;
+  }
+
+  // update variables
+  timer_lcd_voltage ++;
+  prev_voltage = voltage;
+  }
