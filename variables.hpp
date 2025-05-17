a3156cbd5581fa70e74dcc9508236d02194fe5c7
Henry Pickersgill <htap2@live.co.uk>
2025-05-17 17:42:03 +0100

    indenting

--- variables.hpp
+++ variables.hpp
@@ -1,14 +1,14 @@
 // arrays of HIGHs and LOWs to define seven-segment numbers
-int zero[8] = {1, 1, 1, 1, 1, 1, 0};
-int one[8] = {0, 1, 1, 0, 0, 0, 0};
-int two[8] = {1, 1, 0, 1, 1, 0, 1};
+int zero[8]  = {1, 1, 1, 1, 1, 1, 0};
+int one[8]   = {0, 1, 1, 0, 0, 0, 0};
+int two[8]   = {1, 1, 0, 1, 1, 0, 1};
 int three[8] = {1, 1, 1, 1, 0, 0, 1};
-int four[8] = {0, 1, 1, 0, 0, 1, 1};
-int five[8] = {1, 0, 1, 1, 0, 1, 1};
-int six[8] = {1, 0, 1, 1, 1, 1, 1};
+int four[8]  = {0, 1, 1, 0, 0, 1, 1};
+int five[8]  = {1, 0, 1, 1, 0, 1, 1};
+int six[8]   = {1, 0, 1, 1, 1, 1, 1};
 int seven[8] = {1, 1, 1, 0, 0, 0, 0};
 int eight[8] = {1, 1, 1, 1, 1, 1, 1};
-int nine[8] = {1, 1, 1, 1, 0, 1, 1};
+int nine[8]  = {1, 1, 1, 1, 0, 1, 1};
 
 // pins controlling the four digits
 int digit_pins[4] = {2, 3, 4, 5};
